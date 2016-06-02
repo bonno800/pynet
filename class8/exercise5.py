4b6a41900956eac136947ca280b107452c4d7bc4
bonno800 <bonno800@yaho.com>
2016-06-02 06:48:19 -0700

    adding timer

--- class8/exercise5.py
+++ class8/exercise5.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from netmiko import ConnectHandler
 from datetime import datetime
 
@@ -6,6 +8,9 @@ import django
 
 def main():
     django.setup()
+    
+    start_time = datetime.now()
+
 
     devices = NetworkDevice.objects.all()
     for a_device in devices:
@@ -20,7 +25,9 @@ def main():
         print device_type, port, ip, username, password
         remote_conn = ConnectHandler(device_type=device_type, ip=ip, username=username, password=password, port=port)
 	print remote_conn.send_command_expect("show ver")
-        
+
+    elapsed_time = datetime.now() - start_time
+    print "Elapsed time: {}".format(elapsed_time)       
 
 
 if __name__ == "__main__":
