805e94be7f5e57af5b69cb72ed446ed968ae6054
bonno800 <bonno800@yaho.com>
2016-06-10 07:50:00 -0700

    exercise6 redo

--- class8/exercise6.py
+++ class8/exercise6.py
@@ -4,10 +4,11 @@ from datetime import datetime
 from net_system.models import NetworkDevice, Credentials
 import django
 
+import Queue
 import threading
 import time
 
-def show_ver(a_device):
+def show_ver(q, a_device):
     creds = a_device.credentials
     remote_conn = ConnectHandler(device_type=a_device.device_type, 
                                  ip=a_device.ip_address,
@@ -17,22 +18,29 @@ def show_ver(a_device):
     print
     print '#' * 80
     print remote_conn.send_command_expect("show ver")
+
+q = Queue.Queue
+
 def main():
     django.setup()
     start_time = datetime.now()
     devices = NetworkDevice.objects.all()
 
     for a_device in devices:
-        my_thread = threading.Thread(target=show_ver, args=(a_device,))
+        my_thread = threading.Thread(target=show_ver, args=(q,a_device))
         my_thread.start()
+        print threading.currentThread().getName(), 'Starting'
+        time.sleep(1)
+        print threading.currentThread().getName(), 'Exiting'
 
-    main_thread = threading.currentThread()
-    for some_thread in threading.enumerate():
-        if some_thread != main_thread:
-            print some_thread
-            some_thread.join()
+  #  main_thread = threading.currentThread()
+   # for some_thread in threading.enumerate():
+    #    if some_thread != main_thread:
+     #       print some_thread
 
     print "\nElapsed time: " + str(datetime.now() - start_time)
+    s = q.get()
+    print s
 
 if __name__ == "__main__":
     main()
